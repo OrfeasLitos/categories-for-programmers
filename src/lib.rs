eadf8b7bbcb0279abf946d996d8c2ecbb5eca956
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2020-05-07 02:16:52 +0300

    Solve ex. 1

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub fn id<T>(x: T) -> T {
+    x
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use categories::id;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}, {}, {}, {}", id('a'), id(1), id(6.4), id(true));
 }
