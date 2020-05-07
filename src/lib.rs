f30dcd86547754dce9d29bedf46b58066b74ebc0
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2020-05-07 17:07:02 +0300

    Solve ex. 3

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,19 @@ pub fn id<T>(x: T) -> T {
 pub fn o<'a, R, S, T>(f: &'a (dyn Fn(S) -> T), g: &'a (dyn Fn(R) -> S)) -> impl Fn(R) -> T + 'a {
     move |x| f(g(x))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn composable_identity() {
+        fn dummy(x: usize) -> usize {
+            x + 1
+        }
+
+        assert_eq!(dummy(1), o(&dummy, &id)(1));
+        assert_eq!(dummy(1), o(&id, &dummy)(1));
+        assert_eq!(o(&dummy, &id)(1), o(&id, &dummy)(1));
+    }
+}
