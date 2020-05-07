c3e2351d868d5a74878c8c020bec8f3db06f9ebb
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2020-05-07 17:06:41 +0300

    Solve ex. 2

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
 pub fn id<T>(x: T) -> T {
     x
 }
+
+pub fn o<'a, R, S, T>(f: &'a (dyn Fn(S) -> T), g: &'a (dyn Fn(R) -> S)) -> impl Fn(R) -> T + 'a {
+    move |x| f(g(x))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
-use categories::id;
+use categories::{id, o};
+use std::convert::TryInto;
 
 fn main() {
     println!("{}, {}, {}, {}", id('a'), id(1), id(6.4), id(true));
+
+    fn f(x: usize) -> f64 {
+        x as f64 + 1.0
+    }
+
+    fn g(x: isize) -> usize {
+        if x < 0 {
+            (-x).try_into().unwrap()
+        } else {
+            x.try_into().unwrap()
+        }
+    }
+
+    println!("{}, {}, {}", g(-1), f(g(-1)), o(&f, &g)(-1))
 }
