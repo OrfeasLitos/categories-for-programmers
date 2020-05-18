eb99f0ad534830582ba87af23038965985d6de37
Orfeas Litos <o.thyfronitis@ed.ac.uk>
2020-05-18 07:31:29 +0300

    Implement memoize() without struct

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,23 @@ impl<'a, R: Eq + Hash + Clone, S: Clone> Memoized<'_, R, S>
     }
 }
 
+pub fn memoize<R: Eq + Hash, S: Clone>(f: &(dyn Fn(&R) -> S)) -> Box<(dyn FnMut(R) -> S)> {
+    let mut map: HashMap<R, S> = HashMap::new();
+
+    Box::new(
+        move |x: R| {
+            if let Some(y) = map.get(&x) {
+                println!("memoized!");
+                y.clone()
+            } else {
+                let y = f(&x);
+                map.insert(x, y.clone());
+                y
+            }
+        }
+    )
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -52,7 +69,7 @@ mod tests {
     }
 
     #[test]
-    fn memoization() {
+    fn struct_memoize() {
         fn f(x: &isize) -> f64 {
             *x as f64 / 32.0
         }
@@ -60,5 +77,16 @@ mod tests {
         let mut mem_f = Memoized::new(&f);
         assert_eq!(mem_f.exe(42), f(&42));
         assert_eq!(mem_f.exe(42), mem_f.exe(42));
+    }
+
+    #[test]
+    fn func_memoize() {
+        fn f(x: &isize) -> f64 {
+            *x as f64 / 32.0
+        }
+
+        let mut mem_f = memoize(&f);
+        assert_eq!(mem_f(42), f(&42));
+        assert_eq!(mem_f(42), mem_f(42));
     }
 }
